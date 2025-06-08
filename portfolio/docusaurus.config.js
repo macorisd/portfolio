954bc6dbb6f3d8036cf4c48f55f1e7bdbcda40c9
macorisd <macorisd@uma.es>
2025-06-08 22:32:21 +0200

    Fix small bugs

--- portfolio/docusaurus.config.js
+++ portfolio/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://macorisd.github.io',
+  url: 'https://macorisd.github.io/portfolio',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/portfolio/',
