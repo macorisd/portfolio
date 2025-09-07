604f6a73a9698c3927b6bed7e569f321b6c469f9
macorisd <macorisd@uma.es>
2025-09-07 16:10:44 +0200

    Allow all origins in the api

--- server/api.py
+++ server/api.py
@@ -51,7 +51,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Adjust this to your frontend URL
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
